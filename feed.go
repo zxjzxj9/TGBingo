bc2b99dff6042e78790f435d5856d27bfe4aa0c8
Victor Zhang <zxjzxj9@gmail.com>
2022-02-15 21:28:01 +0800

    Add the feed info

--- feed.go
+++ feed.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"github.com/mmcdole/gofeed"
+	"strings"
 )
 
-func getFeed(url string) {
+func getFeed(url string) string {
+	// url e.g. "http://www3.nhk.or.jp/rss/news/cat0.xml"
 	fp := gofeed.NewParser()
-	feed, _ := fp.ParseURL("http://www3.nhk.or.jp/rss/news/cat0.xml")
-	fmt.Println(feed.Title)
-	fmt.Println(feed.Links)
+	feed, _ := fp.ParseURL(url)
+	var sb strings.Builder
 	for _, item := range feed.Items {
-		fmt.Println(item.Title)
+		sb.WriteString(item.Title + "\n")
+		sb.WriteString(item.Links[0] + "\n")
 	}
+	return sb.String()
 }
