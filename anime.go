ac8207cfefac3a83be80de1c07fb5f32a7e0b7e0
Victor Zhang <zxjzxj9@gmail.com>
2022-02-19 20:46:29 +0800

    Add some image decode code

--- anime.go
+++ anime.go
@@ -1,5 +1,17 @@
 package main
 
-func animeGAN() {
+import (
+	"fmt"
+	"image"
+	"io"
+)
 
+func animeGAN(r io.Reader) []byte {
+	//encode jpeg to arryy
+	img, _, err := image.Decode(r)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(img)
+	return nil
 }
