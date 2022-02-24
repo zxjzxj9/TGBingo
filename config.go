0d00409ddd4539ff3a33983e533ac9306be6779f
Victor Zhang <zxjzxj9@gmail.com>
2022-02-24 21:58:56 +0800

    add config file reader

--- config.go
+++ config.go
@@ -1,6 +1,30 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 type Config struct {
 	Server  string `json:"server""`
 	TGToken string `json:"tg_token"`
+}
+
+func loadConfig(fname string) *Config {
+	var config Config
+	f, err := os.Open(fname)
+	if err != nil {
+		fmt.Printf("Error in opening config file: %v", err.Error())
+	}
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Printf("Error in reading data: %v", err.Error())
+	}
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		fmt.Printf("Json unmarshal failed: %v", err.Error())
+	}
+	return &config
 }
