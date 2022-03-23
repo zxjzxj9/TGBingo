7dc2e5e234e8c15be0e6e6a4cc18856b6ce3037d
Victor Zhang <zxjzxj9@gmail.com>
2022-03-23 22:09:33 +0800

    add some example url

--- weather.go
+++ weather.go
@@ -1,5 +1,10 @@
 package main
 
+import "fmt"
+
 func getWeather(city string) string {
+	url1 := "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}"
+	url2 := "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+	fmt.Println(url1, url2)
 	return "It's sunny in " + city
 }
