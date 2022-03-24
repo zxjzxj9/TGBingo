b3e503e964e0a493292a431d79452b889f3602ea
Victor Zhang <zxjzxj9@gmail.com>
2022-03-24 21:34:01 +0800

    add an example of get geo positions

--- weather.go
+++ weather.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-func getWeather(city string) string {
-	url1 := "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}"
+func getWeather(city string, appid string) string {
+	url1 := "http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=1&appid=%s"
 	url2 := "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+	_, _ = http.Get(fmt.Sprintf(url1, city, appid))
 	fmt.Println(url1, url2)
 	return "It's sunny in " + city
 }
