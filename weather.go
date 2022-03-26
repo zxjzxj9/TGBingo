2edb9ab875049f770d59e188feac0ffb80b846d1
Victor Zhang <zxjzxj9@gmail.com>
2022-03-26 21:24:18 +0800

    add error handling

--- weather.go
+++ weather.go
@@ -8,7 +8,10 @@ import (
 func getWeather(city string, appid string) string {
 	url1 := "http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=1&appid=%s"
 	url2 := "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
-	_, _ = http.Get(fmt.Sprintf(url1, city, appid))
-	fmt.Println(url1, url2)
+	ret, err := http.Get(fmt.Sprintf(url1, city, appid))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(url1, url2, ret)
 	return "It's sunny in " + city
 }
