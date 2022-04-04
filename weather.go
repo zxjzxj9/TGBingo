e46911f9d34b72124aaf9ccaad72e73baf28c79e
Victor Zhang <zxjzxj9@gmail.com>
2022-04-04 20:04:54 +0800

    add the temperature and other metrics

--- weather.go
+++ weather.go
@@ -78,5 +78,8 @@ func GetWeather(city string, appid string) string {
 		return "weather request failed"
 	}
 	fmt.Println(url1, url2, ret)
-	return fmt.Sprintf("It's %s in %s, %s", weather.Weather[0].Description, weather.Name, weather.Sys.Country)
+	return fmt.Sprintf("It's %s in %s, temperature %3.1f °C ~ %3.1f °C, feels like %3.1f °C, pressure %d hPa, humidity %d%%",
+		weather.Weather[0].Description, weather.Sys.Country,
+		weather.Main.TempMin-273.15, weather.Main.TempMax-273.15,
+		weather.Main.FeelsLike-273.15, weather.Main.Pressure, weather.Main.Humidity)
 }
