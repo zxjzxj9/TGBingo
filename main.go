6ef3e18383b3236a33de1e963c70b2cb8c50e673
Victor Zhang <zxjzxj9@gmail.com>
2022-04-06 21:45:15 +0800

    add weather enquiry function

--- main.go
+++ main.go
@@ -288,8 +288,13 @@ Help:
     /book {bookid} -- download a book
 	/sensor -- current room status
 	/search_song {song info} -- search a song and generate download link
+	/weather {city} -- current weather in the city
 ####################
 `)
+		} else if strings.HasPrefix(strings.Trim(chatInfo.Message.Text, " \n"), "/weather") {
+			sendMsg(chatInfo.Message.Chat.ID, GetWeather(
+				strings.Trim(strings.Replace(chatInfo.Message.Text, "/weather", "", -1), " \n"),
+				ConfigData.WeatherToken))
 		} else if strings.HasPrefix(strings.Trim(chatInfo.Message.Text, " \n"), "/nhknews") {
 			for _, rss := range getFeed("http://www3.nhk.or.jp/rss/news/cat0.xml") {
 				sendMsg(chatInfo.Message.Chat.ID, rss)
