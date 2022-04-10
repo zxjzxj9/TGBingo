12165413bbf19bc97aae42bee2a1672a42ea114e
Victor Zhang <zxjzxj9@gmail.com>
2022-04-10 18:27:43 +0800

    add get location code

--- aqi.go
+++ aqi.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 func GetAQI(city string, appid string) string {
+	url1 := "http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=1&appid=%s"
+	url2 := "https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s"
+	ret, err := http.Get(fmt.Sprintf(url1, city, appid))
+	if err != nil {
+		fmt.Println(err)
+		return "geology location request failed"
+	}
+
+	// Location part is same as the one in the GetWeather function
+	location := Location{}
+	err = json.NewDecoder(ret.Body).Decode(&location)
+	if err != nil {
+		fmt.Println(err)
+		return "geology request decode failed"
+	}
+	ret, err = http.Get(fmt.Sprintf(url2, location[0].Lat, location[0].Lon, appid))
 	return ""
 }
