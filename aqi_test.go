d3f67b4ff276ee63b285a141d20988ae2f2aa24d
Victor Zhang <zxjzxj9@gmail.com>
2022-04-16 21:29:21 +0800

    add TestGetAQI code

--- aqi_test.go
+++ aqi_test.go
@@ -1,7 +1,15 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestGetAQI(t *testing.T) {
 	// test the GetAQI function
+	ConfigData, err := loadConfig("./config.json")
+	assert.Equal(t, err, nil, "Load config file error!")
+	ret := GetAQI("Shanghai", ConfigData.WeatherToken)
+	fmt.Println(ret)
 }
