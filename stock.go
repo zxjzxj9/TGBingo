d56be45effae9ce426e5fb4f11f6048f7d356b30
Victor Zhang <zxjzxj9@gmail.com>
2022-04-27 21:09:51 +0800

    fix the rsi bug

--- stock.go
+++ stock.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/markcheno/go-quote"
-	"github.com/markcheno/go-talib"
 	"time"
 )
 
@@ -11,6 +10,6 @@ func GetQuote(symbol string) (string, error) {
 	date := time.Now().Format("2006-01-02")
 	spy, _ := quote.NewQuoteFromYahoo(symbol, date, date, quote.Daily, true)
 	fmt.Print(spy.CSV())
-	talib.Rsi(spy.Close, 2)
+	// talib.Rsi(spy.Close, 2)
 	return "", nil
 }
