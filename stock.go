8587336df57ec7f302b81985c9cd437cb36b91da
Victor Zhang <zxjzxj9@gmail.com>
2022-04-29 21:42:14 +0800

    add error handeling

--- stock.go
+++ stock.go
@@ -7,7 +7,10 @@ import (
 
 func GetQuote(symbol string) (string, error) {
 	date := time.Now().Format("2006-01-02")
-	spy, _ := quote.NewQuoteFromYahoo(symbol, date, date, quote.Daily, true)
+	spy, err := quote.NewQuoteFromYahoo(symbol, date, date, quote.Daily, true)
+	if err != nil {
+		return "", err
+	}
 	// fmt.Print(spy.CSV())
 	// talib.Rsi(spy.Close, 2)
 	return spy.CSV(), nil
