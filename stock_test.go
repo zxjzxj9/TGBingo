05ccb715d56724f8676d880f2e3fe177d11382ed
Victor Zhang <zxjzxj9@gmail.com>
2022-05-05 22:34:34 +0800

    Add test functions for get stock quotes

--- stock_test.go
+++ stock_test.go
@@ -2,8 +2,18 @@ package main
 
 import "testing"
 
-func TestGetStockQuotes(t *testing.T) {
+func TestGetStockQuote(t *testing.T) {
 	quotes, err := GetQuote("AAPL")
+	if err != nil {
+		t.Errorf("Error getting stock quotes: %s", err)
+	}
+	if len(quotes) == 0 {
+		t.Errorf("No quotes returned")
+	}
+}
+
+func TestGetStockQuotes(t *testing.T) {
+	quotes, err := GetQuotes([]string{"AAPL", "GOOG"})
 	if err != nil {
 		t.Errorf("Error getting stock quotes: %s", err)
 	}
